16b6e5b1bdea8d3f77b88752da2e6282720e31ff
Baigho Fakku <baigho123@gmail.com>
2023-07-23 20:32:56 +0700

    - feat ✨: new API URL

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,5 +1,5 @@
-const apiUrl = "http://localhost:3000";
+const apiUrl = "https://dtc-test-api.onrender.com/";
 //"https://mbs-mockoon.herokuapp.com";
-// http://localhost:3000
+// http://localhost:3000 
 // https://bms-back-office-api.onrender.com"
 export { apiUrl };
